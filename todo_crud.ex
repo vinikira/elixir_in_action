109cb83b9ef99ceadcf4c5ca20113b7e13095bed
Vinícius Simões <vinicius.simoe95@gmail.com>
2020-07-31 16:48:51 -0300

    [add] 4.2.2 todo_crud.ex updater

--- todo_crud.ex
+++ todo_crud.ex
@@ -18,4 +18,23 @@ defmodule TodoList do
     end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_func) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_func.(old_entry)
+
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+
+        %__MODULE__{todo_list | entries: new_entries}
+    end
+  end
 end
