535c2881b58dbf068dee068ec6aa5e357672be8f
Vinícius Simões <vinicius.simoe95@gmail.com>
2020-08-17 17:32:02 -0300

    [mod] 7.3.2 using todo database

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -3,8 +3,8 @@ defmodule Todo.Server do
 
   # Client stuff
 
-  def start() do
-    GenServer.start(__MODULE__, Todo.List.new())
+  def start(todo_list_name) do
+    GenServer.start(__MODULE__, todo_list_name)
   end
 
   def add_entry(server_pid, entry) do
@@ -26,35 +26,44 @@ defmodule Todo.Server do
   # Server stuff
 
   @impl GenServer
-  def init(initial_state) do
-    {:ok, initial_state}
+  def init(name) do
+    send(self(), :real_init)
+
+    {:ok, name}
   end
 
   @impl GenServer
-  def handle_cast({:add_entry, entry}, todo_list) do
+  def handle_info(:real_init, name) do
+    {:noreply, {name, Todo.Database.get(name) || Todo.List.new()}}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
     new_todo_list = Todo.List.add_entry(todo_list, entry)
 
-    {:noreply, new_todo_list}
+    Todo.Database.store(name, new_todo_list)
+
+    {:noreply, {name, new_todo_list}}
   end
 
   @impl GenServer
-  def handle_cast({:update_entry, entry}, todo_list) do
+  def handle_cast({:update_entry, entry}, {name, todo_list}) do
     new_todo_list = Todo.List.update_entry(todo_list, entry)
 
-    {:noreply, new_todo_list}
+    {:noreply, {name, new_todo_list}}
   end
 
   @impl GenServer
-  def handle_cast({:delete_entry, entry_id}, todo_list) do
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
     new_todo_list = Todo.List.delete_entry(todo_list, entry_id)
 
-    {:noreply, new_todo_list}
+    {:noreply, {name, new_todo_list}}
   end
 
   @impl GenServer
-  def handle_call({:entries, date}, _from, todo_list) do
+  def handle_call({:entries, date}, _from, {name, todo_list}) do
     entries = Todo.List.entries(todo_list, date)
 
-    {:reply, entries, todo_list}
+    {:reply, entries, {name, todo_list}}
   end
 end
